e308319b0e8cedff1de01522f860c50c2df20531
Vasiliy Evdokimov <v.i.evdokimov@yandex.ru>
2024-01-08 01:06:13 +0300

    GTest 1

--- test_version.cpp
+++ test_version.cpp
@@ -1,17 +1,19 @@
-// #define BOOST_TEST_MODULE test_version
+/*
+#define BOOST_TEST_MODULE test_version
 
-// #include "lib.h"
+#include "lib.h"
 
-// #include <boost/test/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
 
-// BOOST_AUTO_TEST_SUITE(test_version)
+BOOST_AUTO_TEST_SUITE(test_version)
 
-// BOOST_AUTO_TEST_CASE(test_valid_version)
-// {
-//     BOOST_CHECK(version() != 100);
-// }
+BOOST_AUTO_TEST_CASE(test_valid_version)
+{
+    BOOST_CHECK(version() != 100);
+}
 
-// BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+*/
 
 #include "lib.h"
 
@@ -24,3 +26,21 @@ int main (int, char **)
     
     return 0;
 }
+
+/*
+#include <gtest/gtest.h>
+
+#include "lib.h"
+
+TEST(Version, Valid) 
+{    
+    int ver = version();
+    ASSERT_LT(ver, 1000);
+}
+
+int main(int argc, char** argv) 
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+*/
