42bbe3c11410e438a5253500d00a3a70d6049cab
Vasiliy Evdokimov <v.i.evdokimov@yandex.ru>
2024-01-08 02:00:08 +0300

    HW1

--- test_version.cpp
+++ test_version.cpp
@@ -1,20 +1,3 @@
-/*
-#define BOOST_TEST_MODULE test_version
-
-#include "lib.h"
-
-#include <boost/test/unit_test.hpp>
-
-BOOST_AUTO_TEST_SUITE(test_version)
-
-BOOST_AUTO_TEST_CASE(test_valid_version)
-{
-    BOOST_CHECK(version() != 100);
-}
-
-BOOST_AUTO_TEST_SUITE_END()
-*/
-
 #include "lib.h"
 
 #include <iostream>
@@ -26,21 +9,3 @@ int main (int, char **)
     
     return 0;
 }
-
-/*
-#include <gtest/gtest.h>
-
-#include "lib.h"
-
-TEST(Version, Valid) 
-{    
-    int ver = version();
-    ASSERT_LT(ver, 1000);
-}
-
-int main(int argc, char** argv) 
-{
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
-*/
